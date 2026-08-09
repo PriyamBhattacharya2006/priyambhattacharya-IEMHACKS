bd75d5cb90669d07400af1a259515ef64b48b49b
Akash0z <akashgoswami112006@gmail.com>
2026-08-09 13:36:51 +0530

    refactored code

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 // ==========================================
 
 
-const GEMINI_API_KEY = "GEMINI_API_KEY";
+const GEMINI_API_KEY = "";
 const GEMINI_MODEL = "gemini-2.5-flash-lite";
 
 // Global Application State Object
